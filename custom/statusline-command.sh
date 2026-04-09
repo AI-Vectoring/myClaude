b8a15a55d4fb3594c425c12b4def223b9808fa33
Martech-WorkShop <martech.ws>
2026-04-09 19:03:14 +0200

    Refactor statusline: each field self-contained for easy add/remove/reorder

--- custom/statusline-command.sh
+++ custom/statusline-command.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 input=$(cat)
 
-model=$(echo "$input" | jq -r '.model.display_name // "?"')
-ctx=$(echo "$input" | jq -r '(.context_window.used_percentage | tostring) + "%"')
-limit5=$(echo "$input" | jq -r '(.rate_limits.five_hour.used_percentage | round | tostring) + "%"')
-limit7=$(echo "$input" | jq -r '(.rate_limits.seven_day.used_percentage | round | tostring) + "%"')
-reset=$(echo "$input" | jq -r '.rate_limits.five_hour.resets_at | strflocaltime("%H:%M")')
-tok_in=$(echo "$input" | jq -r '.context_window.total_input_tokens // 0')
-tok_out=$(echo "$input" | jq -r '.context_window.total_output_tokens // 0')
+fields=()
+fields+=( "$(echo "$input" | jq -r '.model.display_name // "?"')" )
+fields+=( "ctx: $(echo "$input" | jq -r '(.context_window.used_percentage | tostring) + "%"')" )
+fields+=( "limit 5h: $(echo "$input" | jq -r '(.rate_limits.five_hour.used_percentage | round | tostring) + "%"')" )
+fields+=( "limit 7d: $(echo "$input" | jq -r '(.rate_limits.seven_day.used_percentage | round | tostring) + "%"')" )
+fields+=( "reset: $(echo "$input" | jq -r '.rate_limits.five_hour.resets_at | strflocaltime("%H:%M")')" )
+fields+=( "tokin: $(echo "$input" | jq -r '.context_window.total_input_tokens // 0')" )
+fields+=( "tokout: $(echo "$input" | jq -r '.context_window.total_output_tokens // 0')" )
 
-printf "\033[2;37m%s  |  ctx: %s  |  limit 5h: %s  |  limit 7d: %s  |  reset: %s  |  tokin: %s  |  tokout: %s\033[0m" \
-    "$model" "$ctx" "$limit5" "$limit7" "$reset" "$tok_in" "$tok_out"
+output=$(printf '%s  |  ' "${fields[@]}")
+output="${output%  |  }"
+
+printf "\033[2;37m%s\033[0m" "$output"
