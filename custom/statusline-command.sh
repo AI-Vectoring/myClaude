6ba011cb80696eaf3144321c593d254bb4a5157b
Martech-WorkShop <martech.ws>
2026-04-09 18:59:51 +0200

    Fix statusline reset time showing UTC instead of local time

--- custom/statusline-command.sh
+++ custom/statusline-command.sh
@@ -5,7 +5,7 @@ model=$(echo "$input" | jq -r '.model.display_name // "?"')
 ctx=$(echo "$input" | jq -r '(.context_window.used_percentage | tostring) + "%"')
 limit5=$(echo "$input" | jq -r '(.rate_limits.five_hour.used_percentage | round | tostring) + "%"')
 limit7=$(echo "$input" | jq -r '(.rate_limits.seven_day.used_percentage | round | tostring) + "%"')
-reset=$(echo "$input" | jq -r '.rate_limits.five_hour.resets_at | strftime("%H:%M")')
+reset=$(echo "$input" | jq -r '.rate_limits.five_hour.resets_at | strflocaltime("%H:%M")')
 tok_in=$(echo "$input" | jq -r '.context_window.total_input_tokens // 0')
 tok_out=$(echo "$input" | jq -r '.context_window.total_output_tokens // 0')
 
